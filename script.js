7470b5d3d155ac924fd4560a6c40ae76fe5c5b8d
Mohamed shawky <34652284+s7awky9@users.noreply.github.com>
2025-04-21 16:11:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
+  // التبديل بين عرض وإخفاء القائمة
   menu.classList.toggle("open");
+
+  // التبديل بين الأيقونة "hamburger" و "close"
   icon.classList.toggle("open");
+
+  // إضافة أو إزالة التمرير
+  document.body.classList.toggle("no-scroll", menu.classList.contains("open"));
 }
